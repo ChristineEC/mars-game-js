c2ab9ef003d8ae3334668e2d8639aaa1371ebead
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-07-26 16:58:10 +0000

    Add function outlines for getting player and computer choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,19 @@ for (let i=0; i < choiceButtons.length; i++) {
 function getPlayerChoice() {
     console.log("You clicked the button!");
 }
+
+function genComputerChoice() {
+    let numComp = Math.floor(Math.random() * 5) +1;
+    console.log(numComp);
+    if numComp = 1 {
+        document.getElementById(computerChoice).innerHTML = "rock"
+    } else if numComp = 2 {
+        document.getElementById(computerChoice).innerHTML = "paper"
+    } else if numComp = 3 {
+        document.getElementById(computerChoice).innerHTML = "scissors"
+    } else if numnumComp = 4 {
+        document.getElementById(computerChoice).innerHTML = "lizard"
+    } else if numComp = 5 {
+        document.getElementById(computerChoice).innerHTML = "spock"
+    }
+}
