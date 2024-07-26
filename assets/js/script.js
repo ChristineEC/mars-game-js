11b537cf07a51f66819939fcf9b863eec5ae4573
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-07-26 16:23:46 +0000

    Add event listeners to playerChoice buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 
+
+
+// generateComputerchoice() {
+
+// }
+
+
+/**Add event listeners to buttons */
+let choiceButtons = document.getElementsByClassName("choiceButton");
+
+for (let i=0; i < choiceButtons.length; i++) {
+    choiceButtons[i].addEventListener('click', getPlayerChoice);
+}
+
+function getPlayerChoice() {
+    console.log("You clicked the button!");
+}
