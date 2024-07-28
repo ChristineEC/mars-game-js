0621f98660550ec708cd400916b2056a33ae3be4
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-07-28 00:22:57 +0000

    Remove name input button. Add code to generate computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,10 @@ function displayPlayerName(event) {
     console.log("testing");
     let playersName = form.elements["playerName"].value;
     let greetingSpan = document.getElementById("greetNewPlayer");
-    greetingSpan.innerHTML = `Greetings ${playersName}!`;
+    greetingSpan.innerHTML = `Greetings ${playersName}! 
+    Click an icon to make your choice. The computer chooses simultaneously.`;
+
 }
-
-
-
-
-
-
-
-
 
 /**Add event listeners to choice buttons */
 let choiceButtons = document.getElementsByClassName("choiceButton");
@@ -28,27 +22,30 @@ for (let i = 0; i < choiceButtons.length; i++) {
 
 //*Display player choice in results section*/
 function getPlayerChoice() {
-    document.getElementById("playerChoice").innerHTML = this.id;
+    document.getElementById("playerChoice").innerHTML = `You chose: ${this.id}`;
 }
 
-
-function getComputerChoice() {
+/** Generate computer choice */
+function generateComputerChoice() {
     let compNumber = Math.floor(Math.random() * 5) + 1;
+    document.getElementById("compNumber").innerHTML = compNumber;
     console.log(compNumber);
-
-    if (compNumber = 1) {
-        document.getElementById("computerChoice").innerHTML = "rock";
-    } else if (compNumber = 2) {
-        document.getElementById("computerChoice").innerHTML = "paper";
-    } else if (compNumber = 3) {
-        document.getElementById("computerChoice").innerHTML = "scissors";
-    } else if (compNumber = 4) {
-        document.getElementById("computerChoice").innerHTML = "lizard";
-    } else if (compNumber = 5) {
-        document.getElementById("computerChoice").innerHTML = "spock";
-    }
 }
 
+// function displayComputerChoice () {
+//     if (compNumber = 1) {
+//         document.getElementById("computerChoice").innerHTML = "rock";
+//     } else if (compNumber = 2) {
+//         document.getElementById("computerChoice").innerHTML = "paper";
+//     } else if (compNumber = 3) {
+//         document.getElementById("computerChoice").innerHTML = "scissors";
+//     } else if (compNumber = 4) {
+//         document.getElementById("computerChoice").innerHTML = "lizard";
+//     } else if (compNumber = 5) {
+//         document.getElementById("computerChoice").innerHTML = "spock";
+//     }
+// }
+// }
 
 function incrementPlayerScore() {
 
